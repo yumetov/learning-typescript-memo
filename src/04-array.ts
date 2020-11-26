5a1bc8444cfe563c5cbbcd0fc5226cb89552145d
Yumeto Inaoka <hey@yume.to>
2020-11-27 00:55:53 +0900

    learn array type

---
+++ src/04-array.ts
@@ -0,0 +1,20 @@
+export {};
+
+let threeSize: number[] = [72, 55, 78];
+console.log(threeSize);
+
+let statuses: string[] = ['Visual', 'Dance', 'Vocal'];
+console.log(statuses);
+
+let dates: number[][] = [
+  [2007, 4, 2],
+  [2011, 7, 8],
+  [2015, 1, 10]
+];
+console.log(dates);
+
+let resource: Array<number> = [2, 4, 11];
+console.log(resource);
+
+let profile: (string | number | boolean)[] = [61, 'Maya', true];
+console.log(profile);
